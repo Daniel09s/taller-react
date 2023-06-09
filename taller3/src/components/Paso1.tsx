07e5213d2e4213ee58a2f51fee89e0477e6b26c8
Daniel09s <daniel0842s@gmail.com>
2023-06-09 18:57:47 -0500

    Paso 1 Completo

--- taller3/src/components/Paso1.tsx
+++ taller3/src/components/Paso1.tsx
@@ -59,7 +59,8 @@ const Paso1 = () =>{
                 <Form onSubmit={formik.handleSubmit}>
 
                     <Form.Group controlId="nombre">
-                        <h3>Ingrese todos los datos</h3>
+                        <h3 style={{fontFamily: 'negrilla'}}>Ingrese todos los datos</h3>
+                        <hr />
                         <Form.Label>Nombre:</Form.Label>
                         <Form.Control 
                         name="nombre"
