ed9644b5872377014a6a0bcb8a775ceb10e3e5c9
Daniel09s <daniel0842s@gmail.com>
2023-06-14 22:25:17 -0500

    FinApp

--- taller3/src/pages/FinApp.tsx
+++ taller3/src/pages/FinApp.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
-const FinApp= () =>{
-    return(
-        <div>
-            <h2>Fin app</h2>
-        </div>
-    );
-};
+function FinApp() {
+
+    const Navigate = useNavigate();
+
+  const nuevoForm = (e) => {
+    e.preventDefault();
+    Navigate("/paso1");    
+}
+
+  return (
+    <>
+        <h1 style={{textAlign: 'center', marginBottom: '10px'}}>Todos los datos han sido enviados!</h1>
+        <Button style={{ width: '20rem', marginTop: '10px', justifyContent: 'center', padding: '10px', color: 'darkblack', fontFamily: 'arial', boxShadow: '5px 5px 5px black', display: 'flex'}} onClick={nuevoForm}  variant="primary"> 
+          Nuevo formulario
+        </Button>
+    </>
+  );
+}
 
 export default FinApp;
